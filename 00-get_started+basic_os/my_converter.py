1be99af1fd1a1c6ef351a67f68ae6d97e32a1f19
ale66 <ale@dcs.bbk.ac.uk>
2024-09-18 23:55:08 +0200

    Initial commit with presentations

---
+++ 00-get_started+basic_os/my_converter.py
@@ -0,0 +1,5 @@
+my_celsius = int(input("Please enter the current temperature in Milan: "))
+
+my_farenheit = (my_celsius * 9 / 5) + 32
+
+print(f"Today we have {my_farenheit} Farenheit degrees in Milan")
