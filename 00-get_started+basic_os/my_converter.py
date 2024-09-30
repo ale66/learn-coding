d79ee26fad29cdf8a736a335109047dc1e4f2b34
amatuo01 <alberto_matuozzo@yahoo.co.uk>
2024-09-30 15:14:00 +0100

    Milano3

--- 00-get_started+basic_os/my_converter.py
+++ 00-get_started+basic_os/my_converter.py
@@ -1,5 +1,5 @@
-my_celsius = int(input("Please enter the current temperature in Milan: "))
+my_celsius = int(input("Please enter the current temperature in Naples: "))
 
 my_farenheit = (my_celsius * 9 / 5) + 32
 
-print(f"Today we have {my_farenheit} Farenheit degrees in Milan")
+print(f"Today we have {my_farenheit} Farenheit degrees in Naples")
