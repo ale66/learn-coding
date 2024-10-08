ccb112e3b64759f232e5c128d455fc1680284a32
ale66 <alessandro.provetti@gmail.com>
2024-10-08 21:52:37 +0100

    PDF files are now created with makepdf.py

---
+++ 30-iterables/in-class-coding.py
@@ -0,0 +1,45 @@
+# In-class work
+
+'''
+# Print a string vertically (one character per line)
+mystring = 'Crystal Palace FC'
+
+for character in mystring:
+    print(character)
+'''
+
+'''
+# print out a list
+fruitlist = ['apple', 'banana', 'cherry']
+
+print(fruitlist)
+'''
+
+'''
+# Alter lists 
+newfruit = 'orange'
+
+fruitlist[1] = newfruit
+
+print(fruitlist)
+'''
+
+
+teams = ['Chelsea FC', 'Arsenal FC', 'Crystal Palace FC', 'West Ham FC']
+
+howmany = len(teams)
+
+# print them as they are
+for single_team in teams:
+    # write here
+    continue
+
+# print w/o the ' FC' part!
+for single_team in teams:
+    # write here
+    continue
+
+# make the change permanent to the list
+for i in range(howmany):
+    # write here
+    continue
