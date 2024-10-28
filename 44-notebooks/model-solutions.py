89b170ac8fa66ea90cafc8b0d05b0495e0a45c39
ale66 <alessandro.provetti@gmail.com>
2024-10-28 22:11:17 +0000

    Corrected notebook-2 + new section on sets+tuples

---
+++ 44-notebooks/model-solutions.py
@@ -0,0 +1,63 @@
+# Model solution for question 8 of exercise set 3
+# Uncomment to run
+
+'''
+list_names = ['Juliet', 'John', 'Jack', 'Joy', 'Jim', 'Josh', 'Julia']
+
+name1 = input('Please enter your name: ')
+
+found = False
+
+for name in list_names:
+    if name == name1:
+        found = True
+
+
+if found == True:
+    print('Congratulations, you are registered already!')
+
+else:
+    print("Sorry, we don't have your name in the list")
+'''
+
+# Model solution for question 15-b of exercise set 3
+# This version represents a slight generalization to arbitrary input prefixes
+# Uncomment to run
+
+
+list = ['car', 'cat', 'spot', 'talk', 'way', 'code', 'dog', 'fly', 'fun', 'road', 'house', 'duck', 'chew', 'list', 'train']
+
+n = len(list)
+
+prefix = input('Type the beginning of the word: ')
+# original version
+# prefix = 'ca'
+
+l = len(prefix)
+
+found =  False
+
+# with 'for' iteration:
+for word in list:
+    if word[ :l] == prefix:
+        found = True
+
+'''
+# for reference:
+# with 'while' iteration
+i=0  
+
+while i < n:
+    # if the beginning of the word coincides with the prefix
+    if list[i][ :l] == prefix:
+        found = True
+    
+    #increment
+    i += 1
+'''
+
+if found == True:
+    print('A word with this prefix exists already!')
+
+else:
+    print("Sorry, we don't have any words with such prefix!")
---
+++ 60-files/access-files-examples.py
@@ -0,0 +1,74 @@
+# make sure that Python runs in the local folder!
+
+MYFILE = './data/incipit.txt'
+
+with open(MYFILE) as f:
+    longstringwithnewlines = f.read()
+
+print(longstringwithnewlines)
+
+'''
+with open(MYFILE) as f:
+  for line in f:
+    print(line)
+'''
+
+
+# include foreigh functions
+'''
+import csv
+
+FILE = './data/biostats.csv'
+
+
+with open(FILE) as f:
+
+	lines = csv.reader(f, delimiter=',')
+
+	for l in lines:
+		print(l)
+'''
+
+'''
+with open(FILE) as f:
+
+    lines = csv.reader(f, delimiter=',')
+
+    print('Interpretation schema:')
+
+    # consume one line of lines
+    header = next(lines)
+    print(header)
+
+    print('These are the values:')
+    for l in lines:
+        print(l)
+'''
+
+
+# import into dictionaries, use the schema on the first line
+
+'''
+with open(FILE) as f:
+
+    lines = csv.DictReader(f,  delimiter=',')
+
+    for l in lines:
+        print(f'Patient: {l}')
+'''
+
+
+# data migration: use a Spanish-language schema
+
+'''
+first_line = ['Name', 'Sex', 'Age', 'Height(in)', 'Weight(lbs)']
+
+mapping_es = ['Nombre', 'Sexo', 'Edad', 'Estatura(in)', 'Peso(lbs)']
+
+with open(FILE) as f:
+
+    lines = csv.DictReader(f,  fieldnames=mapping_es, delimiter=',')
+
+    for l in lines:
+        print(f'Paciente: {l}')
+'''
