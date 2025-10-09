8d3e43eb4e840ee25aea17bf1cf4b0a398be6808
ale66 <alessandro.provetti@gmail.com>
2025-10-09 23:16:26 +0200

    In-class exercise expanded

--- 30-iterables/in-class-coding.py
+++ 30-iterables/in-class-coding.py
@@ -1,27 +1,23 @@
 # In-class work
 
 '''
-# Print a string vertically (one character per line)
+# assigned to
+# Print a string vertically (one character per line) in reverse order
 mystring = 'Crystal Palace FC'
-
-for character in mystring:
-    print(character)
 '''
 
 '''
-# print out a list
+# assigned to
+# print out the single string containing the names of each fruit without spaces
 fruitlist = ['apple', 'banana', 'cherry']
 
 print(fruitlist)
 '''
 
 '''
-# Alter lists 
+# assigned to
+# Alter lists: add newfruit to fruitlist only if not present already 
 newfruit = 'orange'
-
-fruitlist[1] = newfruit
-
-print(fruitlist)
 '''
 
 
@@ -29,17 +25,25 @@ teams = ['Chelsea FC', 'Arsenal FC', 'Crystal Palace FC', 'West Ham FC']
 
 howmany = len(teams)
 
+
+# not assigned
 # print them as they are
 for single_team in teams:
     # write here
     continue
 
+# assigned to
 # print w/o the ' FC' part!
 for single_team in teams:
     # write here
     continue
 
-# make the change permanent to the list
+# assigned to
+# drop the FC part from the list permanently
 for i in range(howmany):
     # write here
     continue
+
+# assigned to
+# Challenge: remove duplicates from a list
+too_many = ['Chelsea FC', 'Arsenal FC', 'Crystal Palace FC', 'West Ham FC', 'Arsenal FC']
