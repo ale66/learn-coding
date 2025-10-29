47f8fca1c96b412d66485c4fabac9d26ba6e98dd
Alessandro Provetti <ale@dcs.bbk.ac.uk>
2025-10-29 14:29:10 +0000

    Functions with executed code

---
+++ 52-notebooks/spreadsheet2dict.py
@@ -0,0 +1,21 @@
+
+my_dictionary = {
+
+    'name': ['andrea', 'tom'],
+    'age': [32, 35]
+}
+
+
+my_other_dict_1 = {
+    'name':'Andrea',
+    'age':32,
+    'City':'London'
+}
+
+my_other_dict_2 = {
+    'name':'Tom',
+    'age':35,
+    'City':'Brighton'
+}
+
+my_data = [my_other_dict_1, my_other_dict_2]
