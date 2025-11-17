c89dce0f54536ad1ea75dcfd7a1e76f64e3f19de
Alessandro Provetti <a.provetti@bbk.ac.uk>
2025-11-17 13:33:54 +0000

    Merge conflict resolved

--- 70-modules/src/test_module_import.py
+++ 70-modules/src/test_module_import.py
@@ -1,13 +1,17 @@
-
+'''
+test local modules: include a set of functions from a separate Python file.
+In this case the module to be imported sits in the same folder
+'''
 
 import import_me as me
 
 
-<<<<<<< HEAD
-me.loadtext('..\data\hamlet.txt', '\n')
-=======
-me.loadtext('C:\\Users\\ale\\Downloads\\git\\learn-coding\\70-modules\\data\\hamlet.txt', '.')
->>>>>>> 7b14885186e1a117995ba282ccae156025756393
+# me.loadtext('..\data\hamlet.txt', '\n')
+
+# for VS Code excution edit the absolute path to the source text
+HAMLET = "C:\\Users\\ale\\Downloads\\git\\learn-coding\\70-modules\\data\\hamlet.txt"
+
+me.loadtext(HAMLET, '.')
 
 # uncomment to run
 # DATA = '../data/biostats.csv'
